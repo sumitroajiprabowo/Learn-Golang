dc73f402df6a334bbe4184c0c14a1d0a323d3ee5
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-03 15:09:34 +0700

    Add type-declarations.go

---
+++ Fundamental/type-declarations.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	type NoKTP string
+
+	type Kebenaran bool
+
+	var noKtpBudi NoKTP = "3327092905950012"
+	var benar Kebenaran = true
+
+	fmt.Println(noKtpBudi)
+	fmt.Println(benar)
+}

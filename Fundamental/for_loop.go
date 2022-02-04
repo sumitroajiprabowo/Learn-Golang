3b7c71a7773d10c7f064c6de9f8cb71bea3d713f
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-04 12:15:59 +0700

    Add for loop

--- Fundamental/for_loop.go
+++ Fundamental/for_loop.go
@@ -15,4 +15,28 @@ func main() {
 	for counter := 1; counter <= 10; counter++ {
 		fmt.Println("perulangan ke", counter)
 	}
+
+	slice := []string{"Danu", "Budi", "Caca"}
+
+	for i := 0; i < len(slice); i++ {
+		fmt.Println(slice[i])
+	}
+
+	// for with range
+	for i, value := range slice {
+		fmt.Println(i, "=", value)
+	}
+
+	// create map
+	person := make(map[string]string)
+
+	person["name"] = "Danu"
+	person["age"] = "20"
+	person["address"] = "Jakarta"
+
+	// looping map
+	for key, value := range person {
+		fmt.Println(key, "=", value)
+	}
+
 }

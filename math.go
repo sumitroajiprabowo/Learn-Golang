ba3e6f878e20231e21bbf16b2475f4a5519f3b31
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-05 00:26:39 +0700

    Learn Math

---
+++ math.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Ceil(12.17))
+	fmt.Println(math.Floor(12.17))
+	fmt.Println(math.Sqrt(16))
+	fmt.Println(math.Pow(2, 3))
+}

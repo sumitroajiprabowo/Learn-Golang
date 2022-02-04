9105ca57b99d9666f3d39395a36d6b3477ad9f53
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-04 12:15:11 +0700

    Add comment.go

---
+++ Fundamental/comment.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+/**
+Create by Sumitro Aji Prabowo
+Hobby : Programming
+Code For : Fundamental/comment.go
+**/
+func main() {
+	fmt.Println("Hello World") // Hello World (comment for single line)
+}
